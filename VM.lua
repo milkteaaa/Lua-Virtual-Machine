a26f84ce7f3bac3381db0b3373de4fcab363fe8c
yuukixdev <nathantherussian@gmail.com>
2020-12-16 23:31:40 +0700

    Update

--- VM.lua
+++ VM.lua
@@ -1,1 +1,11 @@
 --rework coming soon
+
+local bit = bit32 or require "bit"
+if not bit.blshift then
+    bit.blshift = bit.lshift
+    bit.brshift = bit.rshift
+end
+
+local band, brshift = bit.band, bit.brshift
+local tostring, unpack = tostring, unpack or table.unpack
+
